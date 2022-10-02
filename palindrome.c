84969134d71cfc9d26084f545ccae8c8c471bfea
Sushant kumar jha <72967077+sushant135@users.noreply.github.com>
2022-10-02 12:18:20 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
 int main(){
 int n,r,sum=0;
-printf("enter the number to check its palindrome ");
+printf("enter the number to check its palindrome "); //change sentence (1)
 scanf("%d",&n);
 while(n>0){
 r=n%10;
-sum=sum*10+r;
+sum=sum*10+r; 
 n=n/10;
 }
 if(n==sum)
-printf("it's palindrome number ",sum);
+printf("it's palindrome number ",sum); //change sentences (2)
 else
-printf("it is not a palindrome number ");
+printf("it is not a palindrome number "); //change sentence (3)
 
 return 0;
-}
+
+  
+  //add comments of variable (4)
+  // add comment for use of palindrome and what is it. (5)
